4dd741b901b5ab28f26dcb9587db9e92d192dbaa
wonder <waxz@users.noreply.github.com>
2025-09-10 16:33:51 +0800

    fix error in proxy_gost_cf.sh

--- proxy_gost_cf.sh
+++ proxy_gost_cf.sh
@@ -32,7 +32,6 @@ ps -A -o tid,cmd  | grep -v grep | grep "cloudflared tunnel --url localhost:3802
 
 #  gost proxy
 nohup bash -c "gost -L=mws://:38083?enableCompression=true&keepAlive=true&idletimeout=30s&readBufferSize=64KB" > /tmp/gost.2.out 2>&1 &
-# nohup bash -c "while true; do cloudflared tunnel --url localhost:38083   > /tmp/cloudflared.out 2>&1 ;flock -x  /tmp/cloudflared.out  truncate -s 0 /tmp/cloudflared.out;  done " > /tmp/cloudflared.nohup.out 2>&1 &
 nohup bash -c "cloudflared tunnel --url localhost:38083   > /tmp/cloudflared.out 2>&1" > /tmp/cloudflared.nohup.out 2>&1 &
 
 sleep 10
@@ -61,7 +60,7 @@ while [ "true" ];do
 
 
     if [ -s /tmp/cloudflared.out ]; then
-        cloudflared_url_new=$(flock -s  /tmp/cloudflared.out   cat /tmp/cloudflared.out | grep -oE "https://[a-zA-Z0-9.-]+\.trycloudflare\.com");
+        cloudflared_url_new=$(flock -s  /tmp/cloudflared.out -c "cat /tmp/cloudflared.out" | grep -oE "https://[a-zA-Z0-9.-]+\.trycloudflare\.com");
         if [ -z "$cloudflared_url_new" ]; then
             echo "No cloudflared URL found";
         else
@@ -86,7 +85,7 @@ while [ "true" ];do
         fi;
     fi;
 if [ -s /tmp/cloudflared-ssh.out ]; then
-    cloudflared_ssh_url_new=$(flock -s  /tmp/cloudflared-ssh.out   cat /tmp/cloudflared-ssh.out | grep -oE "https://[a-zA-Z0-9.-]+\.trycloudflare\.com");
+    cloudflared_ssh_url_new=$(flock -s  /tmp/cloudflared-ssh.out -c "cat /tmp/cloudflared-ssh.out" | grep -oE "https://[a-zA-Z0-9.-]+\.trycloudflare\.com");
     if [ -z "$cloudflared_ssh_url_new" ]; then
         echo "No cloudflared SSH URL found";
     else
@@ -110,8 +109,8 @@ if [ -s /tmp/cloudflared-ssh.out ]; then
         fi
     fi
 fi;
-if [ -s /tmp/cloudflared-ttyd.out ];
-    cloudflared_ttyd_url_new=$(flock -s  /tmp/cloudflared-ttyd.out   cat /tmp/cloudflared-ttyd.out | grep -oE "https://[a-zA-Z0-9.-]+\.trycloudflare\.com");
+if [ -s /tmp/cloudflared-ttyd.out ]; then
+    cloudflared_ttyd_url_new=$(flock -s  /tmp/cloudflared-ttyd.out -c "cat /tmp/cloudflared-ttyd.out" | grep -oE "https://[a-zA-Z0-9.-]+\.trycloudflare\.com");
     if [ -z "$cloudflared_ttyd_url_new" ]; then
         echo "No cloudflared ttyd URL found";
     else
